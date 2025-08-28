2a7e3e60bfa3ed93c4e5f1ec84d536c3d15168be
kimdodo97 <eh4536@naver.com>
2025-08-28 22:26:34 +0900

    init : project

---
+++ src/main/java/com/devfeed/DevfeedApplication.java
@@ -0,0 +1,13 @@
+package com.devfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevfeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devfeed/DevfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
